2c3689d8912a590607ae89cbad4e9442604d4ce0
zhanar00 <bimurzaeva.zhanar@gmail.com>
2023-12-11 16:05:39 +0600

    initial commit

---
+++ src/main/java/com/project/optiflow/OptiflowApplication.java
@@ -0,0 +1,13 @@
+package com.project.optiflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptiflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptiflowApplication.class, args);
+	}
+
+}
